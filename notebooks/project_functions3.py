82a2bbf7156ef263f9774d0a500b9d4a890633dc
sherwant <113146103+sherwant@users.noreply.github.com>
2022-11-29 21:21:02 -0800

    Completed minor feedback changes for analysis3 and projectfunctions3

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -4,42 +4,60 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import re
 import project_functions1 as pf1
-
+import project_functions2 as pf2
 
 def remove_def(df):
     #Function that removes "SUB","RES","NA" to clean the data to make it more workable
-    df = pd.Dataframe(df)
+    df = pd.DataFrame(df)
     df.drop(df[df['Position'] == 'SUB'].index, inplace = True)
     df.drop(df[df['Position'] == 'RES'].index, inplace = True)
     df.drop(df[df['Position'] == 'NA'].index, inplace = True)
     return df
 
-def arrange_pos(df):
-    for i in range(len(df)):
-        attackers = ['ST','CF','RW','LW','RF','LF','LS','RS']
-        pattern1 = '|'.join(attackers)
-        midfielders = ['RM','LM','CAM','CM','CDM','LAM','RAM','CDM','LDM','RDM','RCM','LCM']
-        pattern2 = '|'.join(midfielders)
-        defenders = ['CB','LCB','RCB','LB','RB','LWB','RWB']
-        pattern3 = '|'.join(defenders)
-        
-        if   df.Position[i].str.contains(pattern1):
-                df["General Position"][i]=="Attacker"
-        elif df.Position[i].str.contains(pattern2):
-                df["General Position"][i]=="Midfielder"
-        elif df.Position[i].str.contains(pattern3):
-                df["General Position"][i]=="Defender"
-        elif df.Position[i].str.contains(['GK']):
-                df["General Position"][i]=="Goalkeeper"
+def remove_exs(df):
+    # Removes excess columns and returns a workable pandas dataframe.
+    
+    df = df.copy().drop(['ID', 'Name', 'Club', 'Value', 'Wage',
+       'Preferred Foot', 'International Reputation', 'Weak Foot',
+       'Skill Moves', 'Body Type', 'Jersey Number', 'Joined',
+       'Contract Valid Until', 'Height', 'Weight', 'GKDiving',
+       'GKHandling', 'GKKicking', 'GKPositioning', 'GKReflexes',
+       'Release Clause','LoanedFrom','OnLoan'], axis=1)
+
+    df.nunique(axis=0)
+    df.describe().apply(lambda s: s.apply(lambda x: format(x,'f')))
+    df.columns.unique()
+    # Calls custom function clean_position()
+    df['Position'] = df['Position'].map(lambda x: pf1.clean_position(x))
+
+    return df
+
+
+'''def arrange_pos(df):
+    attackers = ['ST','CF','RW','LW','RF','LF','LS','RS']
+    pattern1 = '|'.join(attackers)
+    midfielders = ['RM','LM','CAM','CM','CDM','LAM','RAM','CDM','LDM','RDM','RCM','LCM']
+    pattern2 = '|'.join(midfielders)
+    defenders = ['CB','LCB','RCB','LB','RB','LWB','RWB']
+    pattern3 = '|'.join(defenders)
+
+    GenPosition = []
+
+    for i in range(len(df)df):
+        if   str(df.Position[i]) in attackers:
+                GenPosition.append("Attacker")
+        elif str(df.Position[i]) in midfielders:
+                GenPosition.append("Midfielder")
+        elif str(df.Position[i]) in defenders:
+                GenPosition.append("Defender")
+        elif str(df.Position[i]) in (['GK']):
+                GenPosition.append("Goalkeeper")
         else:
-                df["General Position"][i]=="Not Playing"
-        return df
+                GenPosition.append("Not Playing")
+        
 
-   # mylist = ['ST','CF','RW','LW','RF','LF','LS','RS']
-#pattern = '|'.join(mylist)
+    print(len(GenPosition))
+    df['GenPosition'] = GenPosition
+    return df
 
-#pattern
-#'dog|cat|fish'
-
-#frame.a.str.contains(pattern)
-#def arrange_attribute(df) pace shooting defending dribbling passingphysical
+#def arrange_attribute(df) pace shooting defending dribbling passingphysical'''
